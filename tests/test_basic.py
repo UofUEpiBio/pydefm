f6354fde93a2972e397ca58d02cd99f8e59de000
George G. Vega Yon <g.vegayon@gmail.com>
2023-09-19 14:26:02 -0600

    Removing add and subtract from tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -18,12 +18,4 @@ m.print_y(obj)
 def test_version():
     assert m.__version__ == "0.0.1"
 
-
-def test_add():
-    assert m.add(1, 2) == 3
-
-
-def test_sub():
-    assert m.subtract(1, 2) == -1
-
 print("Everything passed")
